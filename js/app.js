ad9f9af28f09a58e7acfe206a31654c75ae5802b
Daniel Pease <daniel.j.pease@gmail.com>
2016-09-28 17:09:05 -0400

    lots of changes

--- js/app.js
+++ js/app.js
@@ -1,9 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
   console.log('this is after the dom loaded');
-
   var $listForm = document.querySelector('.list-maker-form');
+  var $data = (window.location.search).substr(1);
 
+  // console.log($data.split('='));
+// console.log(document.getElementById('sizeCheckbox'))
+console.log(document.getElementById('colorText'))
+  $listForm[0].value = $data.split('=')[1];
+
+  //
 
 });
 
 console.log('this is before the dom loaded');
+
+

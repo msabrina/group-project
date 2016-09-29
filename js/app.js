b97e814eeddb6df303b6be5bc359c6eab206fb6e
Daniel Pease <daniel.j.pease@gmail.com>
2016-09-29 16:45:11 -0400

    minor changes to js file and stuff

--- js/app.js
+++ js/app.js
@@ -1,26 +1,14 @@
+  var $listForm = document.querySelector('.list-maker-form');
+  var url = (window.location.search).substr(1);
+
+function makeArr() {
+  console.log(url.split('&'))
+}
+
+
 document.addEventListener('DOMContentLoaded', function() {
   console.log('this is after the dom loaded');
-  var $listForm = document.querySelector('.list-maker-form');
-  var $data = (window.location.search).substr(1);
-
-<<<<<<< HEAD
-  // console.log($data.split('&')[0].split('=')[1]);
-  // console.log($listForm[0].id)
-  console.log($data.split('&')[0].split('=')[0])
-  $listForm[0].value = $data.split('&')[0].split('=')[0]
-=======
-  // console.log($data.split('='));
-// console.log(document.getElementById('sizeCheckbox'))
-  //console.log(document.querySelector('option'))
-
-  // console.log(window.location.search)
-  console.log($data)
-
-  // $listForm[0].value = $data.split('=')[1];
->>>>>>> ef93df59639c3595228d90454e42aeb3ca07bf29
-
-  //
-
+  makeArr();
 });
 
 console.log('this is before the dom loaded');

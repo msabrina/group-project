76c660d811d4e11436b33d1ba5d633051ef27a82
Daniel Pease <daniel.j.pease@gmail.com>
2016-09-29 17:57:57 -0400

    restructure divs and add js

--- js/app.js
+++ js/app.js
@@ -1,14 +1,33 @@
   var $listForm = document.querySelector('.list-maker-form');
-  var url = (window.location.search).substr(1);
+  var $data = (window.location.search).substr(1);
+  var firstArr = $data.split('&');
+  var finArr = [];
+  var myObj = {};
 
-function makeArr() {
-  console.log(url.split('&'))
-}
+  function makeArray(str) {
+    var tempArr = [];
+    for(var i = 0; i < firstArr.length; i++){
+      if (firstArr[i].charAt(firstArr[i].length-1) === '=') {
+        firstArr[i] += 'on';
+      }
+      tempArr.push(firstArr[i].split('='));
+      finArr = tempArr.join(',').split(',');
+    }
+  }
 
+  function makeObj(arr) {
+    for (var i = 0; i <arr.length; i+=2) {
+      myObj[arr[i]] = arr[i+1];
+    }
+    console.log(myObj);
+  }
 
 document.addEventListener('DOMContentLoaded', function() {
   console.log('this is after the dom loaded');
-  makeArr();
+
+  makeArray();
+  makeObj(finArr);
+
 });
 
 console.log('this is before the dom loaded');
